e7b4a33afa09201438ca5df960d2cf6b4398ab4a
Emmanuel-Lozano <eemanuellozano@gmail.com>
2026-02-28 00:55:05 -0500

    Bassic ejercice 1

---
+++ Programing Logic/NumberisEvenOrNot/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter the number: ");
+var numberString = Console.ReadLine();
+var numberInt = int.Parse(numberString!);
+if (numberInt % 2 == 0)
+{
+    Console.WriteLine($"The number {numberInt}, is Even");
+}
+else
+{
+    Console.WriteLine($"The number {numberInt}, is Odd");
+}
