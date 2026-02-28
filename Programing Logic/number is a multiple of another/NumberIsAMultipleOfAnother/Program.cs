0d92756a2a9371ff5729992aec6749a1491df536
Emmanuel-Lozano <eemanuellozano@gmail.com>
2026-02-28 02:18:24 -0500

    ejercice 4

---
+++ Programing Logic/number is a multiple of another/NumberIsAMultipleOfAnother/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Enter two diferent numbers");
+Console.Write("Enter the first number: ");
+var numberString = Console.ReadLine();
+var numberInt = int.Parse(numberString!);
+Console.Write("Enter the second number: ");
+var number2String = Console.ReadLine();
+var number2Int = int.Parse(number2String!);
+
+if (numberInt % number2Int == 0)
+{
+    Console.WriteLine($"The number: {number2Int}, is a multiple of: {numberInt}");
+    Console.WriteLine("Congratulatión");
+}
+else
+{
+    Console.WriteLine($"The number: {number2Int}, is not a multiple of: {numberInt}");
+    Console.WriteLine("Try again");
+}
